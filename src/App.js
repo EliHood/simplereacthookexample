c779880ec895c39f512ea2fdde8c014b98d9bdb7
Eli <eli@Elis-MacBook-Pro.local>
2019-03-24 15:02:15 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,97 @@
-import React, { Component } from 'react';
+import React, {Component, useState, Fragment} from 'react';
+import UserTable from './components/UserTable'
+import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
+
+    const usersData = [
+        {
+            id: 1,
+            name: 'Tania',
+            username: 'floppydiskette'
+        }, {
+            id: 2,
+            name: 'Craig',
+            username: 'siliconeidolon'
+        }, {
+            id: 3,
+            name: 'Ben',
+            username: 'benisphere'
+        }
+    ]
+
+    const initialFormState = {
+        id: null,
+        name: '',
+        username: ''
+    }
+
+    const addUser = user => {
+        user.id = users.length + 1
+        setUsers([
+            ...users,
+            user
+        ])
+    }
+
+    // when edit is selected on a user, will turn on edit mode and set the current
+    // user
+    const editRow = user => {
+        setEditing(true)
+
+        setCurrentUser({id: user.id, name: user.name, username: user.username})
+    }
+
+    const updateUser = (id, updatedUser) => {
+        setEditing(false)
+
+        setUsers(users.map(user => (user.id === id
+            ? updatedUser
+            : user)))
+    }
+    const deleteUser = id => {
+        setUsers(users.filter(user => user.id !== id))
+    }
+    const [currentUser,setCurrentUser] = useState(initialFormState)
+    const [users, setUsers] = useState(usersData);
+    const [editing,setEditing] = useState(false);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        <div className="container">
+            <h1>React Hook CRUD Demo</h1>
+            <div className="flex-row">
+                <div className="flex-large">
+
+                    {editing
+                        ? (
+                            <Fragment>
+                                <h2>Edit user</h2>
+                                <EditUserForm
+                                    editing={editing}
+                                    setEditing={setEditing}
+                                    currentUser={currentUser}
+                                    updateUser={updateUser}/>
+                            </Fragment>
+
+                        )
+                        : (
+                            <Fragment>
+                                <h2>Add user</h2>
+                                <AddUserForm addUser={addUser}/>
+                            </Fragment>
+                        )}
+                </div>
+                <div className="flex-large">
+                    <h2>View users</h2>
+                    <UserTable users={users} editRow={editRow} deleteUser={deleteUser}/>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default App;
